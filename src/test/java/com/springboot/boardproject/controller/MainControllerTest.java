b4cd26d38c956ff5f460256ba232beae55b9d788
yeongjun <dudwns650395@gmail.com>
2022-12-08 19:20:56 +0900

    #18 - 루트 경로를 게시판 페이지로 forward
    기본 페이지가 게시판 페이지가 되도록 함.
    url을 유지한 채로 넘겨주고 싶으므로 forward로 설정.

---
+++ src/test/java/com/springboot/boardproject/controller/MainControllerTest.java
@@ -0,0 +1,37 @@
+package com.springboot.boardproject.controller;
+
+import com.springboot.boardproject.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Import(SecurityConfig.class)
+@WebMvcTest(MainController.class)
+class MainControllerTest {
+
+    private final MockMvc mvc;
+
+    public MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view] [GET] 홈 리다이렉션")
+    @Test
+    void givenNothing_whenRequestingRootPage_thenRedirectsToArticlePage() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("forward:/articles"))
+                .andExpect(forwardedUrl("/articles"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
