cd6cf25a6f0a68eac3c6b6dda292d98e6e0b76bc
yeongjun <dudwns650395@gmail.com>
2022-12-09 07:24:36 +0900

    #18 - forward 수정 및 정렬

--- src/test/java/com/springboot/boardproject/service/ArticleCommentServiceTest.java
+++ src/test/java/com/springboot/boardproject/service/ArticleCommentServiceTest.java
@@ -31,7 +31,8 @@ class ArticleCommentServiceTest {
 
     @Mock
     private ArticleRepository articleRepository;
-    @Mock private ArticleCommentRepository articleCommentRepository;
+    @Mock
+    private ArticleCommentRepository articleCommentRepository;
 
     @DisplayName("게시글 ID로 조회하면, 해당하는 댓글 리스트를 반환한다.")
     @Test
